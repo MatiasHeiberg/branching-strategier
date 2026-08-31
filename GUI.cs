6a45c8683826657ba6f186a7ceb03f0ad15f1c6d
Martin Nordbø <nordboe@live.dk>
2026-08-31 13:18:51 +0200

    Startup GUI

--- GUI.cs
+++ GUI.cs
@@ -1,11 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace branching_strategi
+﻿namespace branching_strategi
 {
     public class GUI
     {
+        bool flag = false;
+
+        public GUI()
+        {
+            Startup();
+        }
+
+        public void Startup()
+        {
+            while (!flag)
+            {
+                Console.Clear();
+                Console.WriteLine("---- VELKOMMEN TIL LOMMEREGNER 9000 EXTREME ----\n");
+                Console.WriteLine("---- AFSLUT VED AT TRYKKE 'Q' ----");
+                Console.WriteLine("---- INDTAST REGNESTYKKE OG TRYK ENTER FOR UDREGNING' ----\n\n");
+
+                string? userInteraction = PromptUserCalculation();
+
+                if (userInteraction != null)
+                {
+                    // DEL UD I UDREGNINGER
+                    if (userInteraction.ToLower() == "q")
+                    {
+                        flag = true;
+                    }
+                }
+                else
+                {
+                    Startup();
+                }
+            }
+
+        }
+
+        public string? PromptUserCalculation()
+        {
+            Console.Write("Indtast din regnestykke: ");
+            return Console.ReadLine();
+        }
+
+
     }
 }
-    
