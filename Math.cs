b0926d514791c8aaf671be68417d114e880b5310
Matias Heiberg Nielsen <perm_diners7z@icloud.com>
2026-08-31 13:24:09 +0200

    Feature: power of nth

--- Math.cs
+++ Math.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace branching_strategi
 {
-    internal class Math
+    public class Math
     {
+        public int power_nth (int @base, int exponent)
+        {
+            int result = @base;
+            for (int i = 1;  i < exponent; i++)
+            {
+                result *= @base;
+            }
+            return result;
+        }
     }
 }
