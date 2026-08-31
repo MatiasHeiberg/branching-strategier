be7a833f80d14aa4409189e0034fcb0bc4017228
Martin Nordbø <nordboe@live.dk>
2026-08-31 14:20:38 +0200

    Fix to Math.cs

--- Math.cs
+++ Math.cs
@@ -1,26 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace branching_strategi
+﻿namespace branching_strategi
 {
     public class Math
     {
         public int Multiplication(int a, int b)
-        {  return a * b; }
-using System.Linq;
-namespace branching_strategi
-{
-    public class Math
-    {
+        { return a * b; }
+
         public double SquareRoot(double a)
         {
             return System.Math.Sqrt(a);
         }
-        public int power_nth (int @base, int exponent)
+        public int power_nth(int @base, int exponent)
         {
             int result = @base;
-            for (int i = 1;  i < exponent; i++)
+            for (int i = 1; i < exponent; i++)
             {
                 result *= @base;
             }
@@ -32,18 +24,13 @@ namespace branching_strategi
         {
             double result = a / b;
             return result;
-            
+
         }
-    
-
-
-
-
 
         public double Average(List<int> tal)
         {
             return tal.Average();
-        }  
+        }
 
         public double Mudolo(double a, double b)
         {
