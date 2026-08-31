6518817c4ac084b62dd52becbdf02121ddc104a3
Lasse Agerskov <lasse.agerskov@gmail.com>
2026-08-31 13:26:46 +0200

    Average

--- Math.cs
+++ Math.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Linq;
 namespace branching_strategi
 {
     internal class Math
     {
+        public double Average(List<int> tal)
+        {
+            return tal.Average();
+        }  
+
     }
 }
